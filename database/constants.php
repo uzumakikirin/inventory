423d8a3af4d066c2267d500a843bce11e8198b4e
unknown <uzumakikirin@gmail.com>
2019-09-21 17:01:26 +0900

    first commit

---
+++ database/constants.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+define("HOST","localhost");
+define("USER","root");
+define("PASS","");
+define("DB","invdb");
+
+define("DOMAIN","http://localhost/inventory/public_html");
+
+?>
